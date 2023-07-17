5e81dd2471684b90337e8c0362ec3df801bd2aa5
Albert R. Burnyashov <albrburn@gmail.com>
2023-07-17 23:17:39 +0300

    Initial commit

---
+++ app/src/main/java/ru/netology/yandmap/db/PlaceDatabase.kt
@@ -0,0 +1,27 @@
+package ru.netology.yandmap.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import ru.netology.yandmap.dao.PlaceDao
+import ru.netology.yandmap.entity.PlaceEntity
+
+@Database(entities = [PlaceEntity::class], version = 1)
+abstract class PlaceDatabase : RoomDatabase() {
+    abstract val placeDao: PlaceDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: PlaceDatabase? = null
+
+        fun getInstance(context: Context): PlaceDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(context, PlaceDatabase::class.java, "place_db")
+                    .build()
+                    .also {
+                        INSTANCE = it
+                    }
+            }
+    }
+}
